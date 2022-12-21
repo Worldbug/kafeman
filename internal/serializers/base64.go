b92b0d26460ea26da4c978d1357225d621086d36
Tikhomirov Kirill Andreevich <ktikhomirov@ozon.ru>
2022-12-21 13:32:59 +0300

    [update] base64 encoder

--- internal/serializers/base64.go
+++ internal/serializers/base64.go
@@ -1,5 +1,7 @@
 package serializers
 
+import "encoding/base64"
+
 func NewBase64Serializer() *Base64Serializer {
 	return &Base64Serializer{}
 }
@@ -7,9 +9,13 @@ func NewBase64Serializer() *Base64Serializer {
 type Base64Serializer struct{}
 
 func (bs *Base64Serializer) Encode(input []byte) ([]byte, error) {
-	return input, nil
+	encoded := base64.StdEncoding.EncodeToString(input)
+	return []byte(encoded), nil
 }
 
 func (bs *Base64Serializer) Decode(input []byte) ([]byte, error) {
-	return input, nil
+	raw := make([]byte, 0)
+	_, err := base64.StdEncoding.Decode(raw, input)
+
+	return raw, err
 }
