7fe6da1c30cb14c1834a7dab8793612c6b9de9b6
Tikhomirov Kirill Andreevich <ktikhomirov@ozon.ru>
2023-05-01 13:43:17 +0300

    update config init cmd

--- cmd/kafeman/config_cmd/init.go
+++ cmd/kafeman/config_cmd/init.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/worldbug/kafeman/cmd/kafeman/common"
 	"github.com/worldbug/kafeman/cmd/kafeman/run_configuration"
+	"github.com/worldbug/kafeman/internal/config"
 )
 
 func NewConfigInitCMD(configPath string) *cobra.Command {
@@ -15,6 +16,16 @@ func NewConfigInitCMD(configPath string) *cobra.Command {
 		Short: "Create empty config and export to file (default ~/.kafeman/config.yaml)",
 		Run: func(cmd *cobra.Command, args []string) {
 			// TODO: FIXME: config path
+			run_configuration.Config = &config.Configuration{
+				CurrentCluster: "local",
+				Clusters: []config.Cluster{
+					{
+						Name:    "local",
+						Brokers: []string{"localhost:9092"},
+					},
+				},
+			}
+
 			err := run_configuration.WriteConfiguration()
 			if err != nil {
 				common.ExitWithErr("Can`t save config: %+v", err)
